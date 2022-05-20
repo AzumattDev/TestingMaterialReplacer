642615f0d55d12a46b68670f7b0602d97aa44684
AzumattDev <azumattgaming@gmail.com>
2022-05-20 05:03:12 -0400

    Initial commit

---
+++ PieceManager/MaterialReplacer.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using JetBrains.Annotations;
+using TestingMaterialReplacer;
+using UnityEngine;
+
+namespace PieceManager
+{
+    [PublicAPI]
+    public static class MaterialReplacer
+    {
+        static MaterialReplacer()
+        {
+            originalMaterials = new Dictionary<string, Material>();
+            _objectToSwap = new Dictionary<GameObject, bool>();
+            _objectsForShaderReplace = new Dictionary<GameObject, ShaderType>();
+            Harmony harmony = new("org.bepinex.helpers.PieceManager");
+            harmony.Patch(AccessTools.DeclaredMethod(typeof(ObjectDB), nameof(ObjectDB.Awake)),
+                postfix: new HarmonyMethod(AccessTools.DeclaredMethod(typeof(MaterialReplacer),
+                    nameof(GetAllMaterials))));
+            harmony.Patch(AccessTools.DeclaredMethod(typeof(ObjectDB), nameof(ObjectDB.Awake)),
+                postfix: new HarmonyMethod(AccessTools.DeclaredMethod(typeof(MaterialReplacer),
+                    nameof(ReplaceAllMaterialsWithOriginal))));
+        }
+
+        public enum ShaderType
+        {
+            PieceShader,
+            VegetationShader,
+            RockShader,
+            RugShader,
+            GrassShader
+        }
+
+        private static Dictionary<GameObject, bool> _objectToSwap;
+        internal static Dictionary<string, Material> originalMaterials;
+        private static Dictionary<GameObject, ShaderType> _objectsForShaderReplace;
+
+        public static void RegisterGameObjectForShaderSwap(GameObject go, ShaderType type = ShaderType.PieceShader)
+        {
+            _objectsForShaderReplace?.Add(go, type);
+        }
+
+        public static void RegisterGameObjectForMatSwap(GameObject go, bool isJotunnMock = false)
+        {
+            _objectToSwap.Add(go, isJotunnMock);
+        }
+
+        [HarmonyPriority(Priority.VeryHigh)]
+        private static void GetAllMaterials()
+        {
+            var allmats = Resources.FindObjectsOfTypeAll<Material>();
+            foreach (var item in allmats)
+            {
+                originalMaterials[item.name] = item;
+            }
+        }
+
+        [HarmonyPriority(Priority.VeryHigh)]
+        private static void ReplaceAllMaterialsWithOriginal()
+        {
+            if (originalMaterials.Count <= 0) GetAllMaterials();
+            foreach (var renderer in _objectToSwap.SelectMany(gameObject =>
+                         gameObject.Key.GetComponentsInChildren<Renderer>(true)))
+            {
+                _objectToSwap.TryGetValue(renderer.gameObject, out bool jotunnPrefabFlag);
+                for (int i = 0; i < renderer.materials.Length; i++)
+                {
+                    if (jotunnPrefabFlag)
+                    {
+                        if (!renderer.materials[i].name.StartsWith("JVLmock_")) continue;
+                        var matName = renderer.material.name.Replace(" (Instance)", string.Empty)
+                            .Replace("JVLmock_", "");
+
+                        if (originalMaterials.ContainsKey(matName))
+                        {
+                            renderer.material = originalMaterials[matName];
+                        }
+                        else
+                        {
+                            Debug.LogWarning("No suitable material found to replace: " + matName);
+                            // Skip over this material in future
+                            originalMaterials[matName] = renderer.material;
+                        }
+                    }
+                    else
+                    {
+                        if (!renderer.materials[i].name.Contains("_REPLACE_")) continue;
+                        TestingMaterialReplacerPlugin.TestingMaterialReplacerLogger.LogWarning(
+                            $"----BEFORE: {renderer.material.name}");
+                        string matName = renderer.material.name.Replace(" (Instance)", string.Empty)
+                            .Replace("_REPLACE_", string.Empty);
+                        TestingMaterialReplacerPlugin.TestingMaterialReplacerLogger.LogWarning(
+                            $"----AFTER: {matName}");
+
+                        TestingMaterialReplacerPlugin.TestingMaterialReplacerLogger.LogWarning(
+                            $"----BEFORE#2: {renderer.materials[i].name}");
+                        string name = renderer.materials[i].name.Replace(" (Instance)", string.Empty)
+                            .Replace("_REPLACE_", string.Empty);
+                        TestingMaterialReplacerPlugin.TestingMaterialReplacerLogger.LogWarning(
+                            $"----AFTER#2: {name}");
+                        if (originalMaterials.ContainsKey(matName))
+                        {
+                            renderer.material = originalMaterials[matName];
+                        }
+                        else
+                        {
+                            Debug.LogWarning("No suitable material found to replace: " + matName);
+                            // Skip over this material in future
+                            originalMaterials[matName] = renderer.material;
+                        }
+
+                        if (originalMaterials.ContainsKey(name))
+                        {
+                            renderer.materials[i] = originalMaterials[name];
+                        }
+                        else
+                        {
+                            Debug.LogWarning("No suitable material found to replaceSECOND: " + name);
+                            // Skip over this material in future
+                            originalMaterials[name] = renderer.material;
+                        }
+                    }
+                }
+            }
+
+            foreach (var renderer in _objectsForShaderReplace.SelectMany(gameObject =>
+                         gameObject.Key.GetComponentsInChildren<Renderer>(true)))
+            {
+                _objectsForShaderReplace.TryGetValue(renderer.gameObject, out ShaderType shaderType);
+                foreach (var t in renderer.materials)
+                {
+                    switch (shaderType)
+                    {
+                        case ShaderType.PieceShader:
+                            t.shader = Shader.Find("Custom/Piece");
+                            break;
+                        case ShaderType.VegetationShader:
+                            t.shader = Shader.Find("Custom/Vegetation");
+                            break;
+                        case ShaderType.RockShader:
+                            t.shader = Shader.Find("Custom/StaticRock");
+                            break;
+                        case ShaderType.RugShader:
+                            t.shader = Shader.Find("Custom/Rug");
+                            break;
+                        case ShaderType.GrassShader:
+                            t.shader = Shader.Find("Custom/Grass");
+                            break;
+                        default:
+                            t.shader = Shader.Find("ToonDeferredShading2017");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,119 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using PieceManager;
+using ServerSync;
+
+namespace TestingMaterialReplacer
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class TestingMaterialReplacerPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "TestingMaterialReplacer";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+        internal static string ConnectionError = "";
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource TestingMaterialReplacerLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public void Awake()
+        {
+            _serverConfigLocked = config("General", "Force Server Config", true, "Force Server Config");
+            _ = ConfigSync.AddLockingConfigEntry(_serverConfigLocked);
+
+            
+            BuildPiece examplePiece1 = new("testingmreplacer", "ReplacedWoodRoof");
+
+            examplePiece1.Name
+                .English("Replaced Roof"); // Localize the name and description for the building piece for a language.
+            examplePiece1.Description.English("Ward For testing the Piece Manager");
+            examplePiece1.RequiredItems.Add("FineWood", 20,
+                false);
+            examplePiece1.Category.Add(BuildPieceCategory.Building, false);
+
+            MaterialReplacer.RegisterGameObjectForMatSwap(examplePiece1.Prefab);
+            MaterialReplacer.RegisterGameObjectForShaderSwap(examplePiece1.Prefab);
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                TestingMaterialReplacerLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                TestingMaterialReplacerLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                TestingMaterialReplacerLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<bool>? _serverConfigLocked;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+        #endregion
+    }
+}
